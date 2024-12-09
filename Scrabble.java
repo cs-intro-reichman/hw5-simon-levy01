4dd4831fcfe28559f1626e2c03ba3a578c26b185
Simon Levy <simon.levy01@post.runi.ac.il>
2024-12-09 23:58:14 +0200

    attempting scrabble, playhandwithmockinput passes in vscode, just not here

--- Scrabble.java
+++ Scrabble.java
@@ -112,7 +112,8 @@ public class Scrabble {
 			} else if (MyString.subsetOf(input, hand)){
 				score += wordScore(input);
 				hand = MyString.remove(hand,input);
-				System.out.println(input + " earned " + wordScore(input) + " points. Score: " + score + " points");
+				System.out.println(input + " earned " + wordScore(input) + " points. Score: " + score + " points" );
+				System.out.println("");
 			}
 		}
 		if (hand.length() == 0) {
